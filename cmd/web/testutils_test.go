a65ab2b04832d8b46fec61d202f4ea55d5bd13e7
syntacticNaCl <garrettrappaport@protonmail.ch>
2019-03-12 21:08:57 -0400

    Add DB and integration tests

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,18 +1,56 @@
 package main
 
 import (
+	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
+	"time"
+
+	"github.com/syntacticNaCl/snippetbox/pkg/models/mock"
+
+	"github.com/golangcollege/sessions"
 )
 
+// Define a regular expression which captures the CSRF token value from the
+// HTML for our user signup page.
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	// Use the FindSubmatch method to extract the token from the HTML body.
+	// Note that this returns an array with the entire matched pattern in the
+	// first position, and the values of any captured data in the subsequent
+	// positions.
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+	return html.UnescapeString(string(matches[1]))
+}
+
 func newTestApplication(t *testing.T) *application {
+	templateCache, err := newTemplateCache("./../../ui/html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Create a session manager instance, with the same settings as production.
+	session := sessions.New([]byte("3dSm5MnygFHh7XidAtbskXrjbwfoJcbJ"))
+	session.Lifetime = 12 * time.Hour
+	session.Secure = true
+	// Initialize the dependencies, using the mocks for the loggers and
+	// database models.
 	return &application{
-		errorLog: log.New(ioutil.Discard, "", 0),
-		infoLog:  log.New(ioutil.Discard, "", 0),
+		errorLog:      log.New(ioutil.Discard, "", 0),
+		infoLog:       log.New(ioutil.Discard, "", 0),
+		session:       session,
+		snippets:      &mock.SnippetModel{},
+		templateCache: templateCache,
+		users:         &mock.UserModel{},
 	}
 }
 
@@ -53,5 +91,25 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byt
 		t.Fatal(err)
 	}
 
+	return rs.StatusCode, rs.Header, body
+}
+
+// postForm method for sending POST requests to the test server.
+// The final parameter to this method is a url.Values object which can contain
+// any data that we want to send in the request body.
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the response body.
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Return the response status, headers and body.
 	return rs.StatusCode, rs.Header, body
 }
---
+++ pkg/models/mysql/users_test.go
@@ -0,0 +1,66 @@
+package mysql
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/syntacticNaCl/snippetbox/pkg/models"
+)
+
+func TestUserModelGet(t *testing.T) {
+	// Skip the test if the `-short` flag is provided when running the test.
+	if testing.Short() {
+		t.Skip("mysql: skipping integration test")
+	}
+
+	// Set up a suite of table-driven tests and expected results.
+	tests := []struct {
+		name      string
+		userID    int
+		wantUser  *models.User
+		wantError error
+	}{
+		{
+			name:   "Valid ID",
+			userID: 1,
+			wantUser: &models.User{
+				ID:      1,
+				Name:    "Malcolm Reynolds",
+				Email:   "mal@serenity.com",
+				Created: time.Date(2018, 12, 23, 17, 25, 22, 0, time.UTC),
+			},
+			wantError: nil,
+		},
+		{
+			name:      "Zero ID",
+			userID:    0,
+			wantUser:  nil,
+			wantError: models.ErrNoRecord,
+		},
+		{
+			name:      "Non-existent ID",
+			userID:    2,
+			wantUser:  nil,
+			wantError: models.ErrNoRecord,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, teardown := newTestDB(t)
+			defer teardown()
+
+			m := UserModel{db}
+
+			user, err := m.Get(tt.userID)
+
+			if err != tt.wantError {
+				t.Errorf("want %v; got %s", tt.wantError, err)
+			}
+
+			if !reflect.DeepEqual(user, tt.wantUser) {
+				t.Errorf("want %v; got %v", tt.wantUser, user)
+			}
+		})
+	}
+}
